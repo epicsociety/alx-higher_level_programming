24ea02eaea68d534a7afa7d58820fb3ac1bab571
Epicsociety <measkswho@gmail.com>
2023-01-25 02:53:39 +0300

    a private attribute to square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''A Defining the class Square'''
+
+
+class Square:
+    '''A class square
+    Attruiutes: __size (int): size of side of a square'''
+
+    def __init__(self, size):
+        '''Initializes a square
+        Args: size (int):size of a side of a square
+        Returns: None'''
+        self.__size = size
