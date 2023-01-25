011b46c5d55558f0d79ef2ff37861fadacb3be8e
Epicsociety <measkswho@gmail.com>
2023-01-25 15:17:45 +0300

    Printed a list, returned number of elements printed, used try except block

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,9 @@ def safe_print_list(my_list=[], x=0):
     elements_printed = 0
     for i in range(x):
         try:
-            print(my_list[i], end=" ")
+            print("{}".format(my_list[i]), end="")
             elements_printed += 1
         except IndexError:
-            break
+            pass
     print()
     return elements_printed
