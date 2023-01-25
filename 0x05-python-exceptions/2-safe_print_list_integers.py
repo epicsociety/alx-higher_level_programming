8e70d006c44f8764fecab4295e83de314e66e9a1
Epicsociety <measkswho@gmail.com>
2023-01-25 15:48:13 +0300

    more try except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,5 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             n += 1
         except (ValueError, TypeError):
             continue
-        print()
-        return n
+    print()
+    return n
