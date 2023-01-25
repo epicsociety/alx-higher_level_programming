37b2a4d6222667209980263cdafc4c1585e67697
Epicsociety <measkswho@gmail.com>
2023-01-25 18:50:17 +0300

    Raise error and prints a message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,12 +2,10 @@
 
 
 def safe_print_integer_err(value):
+    from sys import stderr
     try:
         print("{:d}".format(value))
-        raise TypeError as ty
-    except TypeError:
-        return False
-        print("Exception:", ty)
-    else:
         return True
-
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=stderr)
+        return False
