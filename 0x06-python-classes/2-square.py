1eed704486a4430f2e2a220f2977a32d94c8a093
Epicsociety <measkswho@gmail.com>
2023-01-25 03:34:53 +0300

    Raise errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-'''A Defining the class Square'''
+"""A Defining the class Square"""
 
 
 class Square:
-    '''A class square
-    Attruiutes: __size (int): size of side of a square'''
+    """A class square"""
 
     def __init__(self, size=0):
-        '''Initializes a square
-        Args:
-            size:size of one side of a square
-        '''
+        """Initializes a square size:size of one side of a square
+        """
 
         if type(size) is int:
             if size >= 0:
                 self.__size = size
             else:
-                raise ValueError("size must be >= 0")
+                raise ValueError('size must be >= 0')
         else:
-            raise TypeError("size must be an interger")
+            raise TypeError('size must be an interger')
