64d84182018bec04fc755220b2ed623c886121b2
Epicsociety <measkswho@gmail.com>
2023-01-26 10:16:19 +0300

    correction

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,13 +8,12 @@ class Square:
     def __init__(self, size=0):
         """
         Args:
-        Size: size of square
+            Size: size of square
         """
 
-        if type(size) is int:
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
-        else:
+        if type(size) is not int:
             raise TypeError('size must be an interger')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
