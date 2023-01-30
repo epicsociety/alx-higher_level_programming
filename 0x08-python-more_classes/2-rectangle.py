f3711e37c71b9077c8927c77c737f6db739a1118
Epicsociety <measkswho@gmail.com>
2023-01-31 00:44:06 +0300

    Area and perimeter of the rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""defines a class"""
+
+
+class Rectangle:
+    """
+    an empty Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        """
+        instantiation with height and weight
+        height: height of rectangle
+        width: width of rectagle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for width a private atrribute"""
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height, a private attribute"""
+
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """area of a rectangle"""
+        return width * height
+    
+    def perimeter(self):
+        """perimeter of the rectangle"""
+
+        if width == 0 or height == 0:
+            perimeter = 0
+        else:
+            perimeter = int((2 * height) + (2 * width))
+
+        return perimeter
