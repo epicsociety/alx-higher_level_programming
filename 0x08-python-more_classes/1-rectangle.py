b48a260d1937fee88f466d46805b98c798b5bea6
Epicsociety <measkswho@gmail.com>
2023-01-31 00:34:38 +0300

    Instantiation of rectangle with height and width

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """setter for width a private atrribute"""
-        if value != int:
+        if type(value) != int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -38,7 +38,7 @@ class Rectangle:
     def height(self, value):
         """setter for height, a private attribute"""
 
-        if value != int:
+        if type(value) != int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
