79777b880f14d3a674008a1651e8c2a5ee7ebb9b
Epicsociety <measkswho@gmail.com>
2023-02-03 19:27:13 +0300

    Tests printing a square

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,3 +6,23 @@
 
 
 def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    for type(text) is not str:
+        raise TypeError('text must be a string')
+
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
