8e039c774c07474cc832d789d27f5376c1669657
Epicsociety <measkswho@gmail.com>
2023-02-08 00:58:51 +0300

    added return

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ def write_file(filename="", text=""):
     writes a string to text file and returns the numbe of characters written
     """
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(text)
+        return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,4 +7,4 @@ def append_write(filename="", text=""):
     appends a string at the end of a text file
     """
     with open(filename, 'a', encoding='utf-8') as f:
-        f.write(text)
+        return f.write(text)
