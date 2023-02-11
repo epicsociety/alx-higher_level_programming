4dc38a20e979052e6a2bfd2c20c8a8e5f04c2ec6
Epicsociety <measkswho@gmail.com>
2023-02-11 10:45:02 +0300

    arrange the folders

--- 0x0C-python-almost_a_circle/0-main.py
+++ 0x0C-python-almost_a_circle/0-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ 0-main """
-from test_models.test_base import Base
+from models.base import Base
 
 if __name__ == "__main__":
     b1 = Base()
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,16 +1,1 @@
 #!/usr/bin/python3
-"""a base class"""
-
-
-class Base:
-    """the definition of the base class"""
-    __nb_objects = 0  #private class attribute
-
-    def __init__(self, id=None):
-        """instantiation of the base class"""
-        if id is not None:
-            self.id = id
-
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
