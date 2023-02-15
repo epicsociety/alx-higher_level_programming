e7be8a9dc3d9bccb7e01408014c6dc069ba82bc1
Epicsociety <measkswho@gmail.com>
2023-02-15 15:01:02 +0300

    test base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,102 +5,22 @@ Unittest classes:
     testBase_instantiation
 """
 
-import os
-import unittest
-from models.base import Base
-Base = Base
-to_json_string = Base.to_json_string
-from_json_string = Base.from_json_string
-save_to_file = Base.save_to_file
-load_from_file = Base.load_from_file
-create = Base.create
 
+class Test_Base_instantiation(unittest.TestCase):
+    """class tests the constructor method"""
 
-class TestBase_instantiation(unittest.TestCase):
-    """unittests for testing instantiation of the Base class."""
+    def test_init_assigns_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
 
-    def test_no_arg(self):
+    def test_init_assigns_unique_id(self):
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b1.id, b1.id - 1)
+        self.assertEqual(b2.id, 2)
 
-    def test_three_bases(self):
-        b1 = Base()
-        b2 = Base()
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 2)
+    def test_init_assigns_given_id(self):
+        b1 = Base(89)
+        self.assertEqual(b1.id, 89)
 
-    def test_None_id(self):
-        b1 = Base(None)
-        b2 = Base(None)
-        self.assertEqual(b1.id, b2.id - 1)
-
-    def test_unique_id(self):
-        self.assertEqual(12, Base(12).id)
-
-    def test_nb_instances_after_unique_id(self):
-        b1 = Base()
-        b2 = Base(12)
-        b3 = Base()
-        self.assertEqual(b1.id, b3.id - 1)
-
-    def test_id_public(self):
-        b = Base(12)
-        b.id = 15
-        self.assertEqual(15, b.id)
-
-    def test_nb_instances_private(self):
-        with self.assertRaises(AttributeError):
-            print(Base(12).__nb__instances)
-
-    def test_str_id(self):
-        self.assertEqual("hello", Base("hello").id)
-
-    def test_float_id(self):
-        self.assertEqual(5.5, Base(5.5).id)
-
-    def test_complex_id(self):
-        self.assertEqual(complex(5), Base(complex(5)).id)
-
-    def test_dict_id(self):
-        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
-
-    def test_bool_id(self):
-        self.assertEqual(True, Base(True).id)
-
-    def test_list_id(self):
-        self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
-
-    def test_tuple_id(self):
-        self.assertEqual((1, 2), Base((1, 2)).id)
-
-    def test_set_id(self):
-        self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
-
-    def test_frozenset_id(self):
-        self.assertEqual(frozenset({1, 2, 3}), Base(frozenset({1, 2, 3})).id)
-
-    def test_range_id(self):
-        self.assertEqual(range(5), Base(range(5)).id)
-
-    def test_bytes_id(self):
-        self.assertEqual(b'Python', Base(b'Python').id)
-
-    def test_bytearray_id(self):
-        self.assertEqual(bytearray(b'abcefg'), Base(bytearray(b'abcefg')).id)
-
-    def test_memoryview_id(self):
-         self.assertEqual(memoryview(b'abcefg'), Base(memoryview(b'abcefg')).id)
-
-    def test_inf_id(self):
-        self.assertEqual(float('inf'), Base(float('inf')).id)
-
-    def test_NaN_id(self):
-        self.assertNotEqual(float('nan'), Base(float('nan')).id)
-
-    def test_two_args(self):
-        with self.assertRaises(TypeError):
-            Base(1, 2)
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == "__main__":
+        unittest.main()
