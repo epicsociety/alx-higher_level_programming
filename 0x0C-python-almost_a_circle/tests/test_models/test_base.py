0cf8758223b7baa82c8a656c6918447e06e01a5b
Epicsociety <measkswho@gmail.com>
2023-02-15 15:26:04 +0300

    test base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,8 @@
 Unittest classes:
     testBase_instantiation
 """
+import unittest
+from models.base import Base
 
 
 class Test_Base_instantiation(unittest.TestCase):
@@ -11,16 +13,28 @@ class Test_Base_instantiation(unittest.TestCase):
 
     def test_init_assigns_id(self):
         b1 = Base()
-        self.assertEqual(b1.id, 1)
+        self.assertEqual(b1.id, b1.id)
 
     def test_init_assigns_unique_id(self):
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b2.id, 2)
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_three_bases_id_assigns(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
 
     def test_init_assigns_given_id(self):
-        b1 = Base(89)
-        self.assertEqual(b1.id, 89)
+        b1 = Base(9)
+        self.assertEqual(b1.id, 9)
+
+    def test_after_unique_id(self):
+        b1 = Base()
+        b2 = Base(9)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
 
     if __name__ == "__main__":
         unittest.main()
