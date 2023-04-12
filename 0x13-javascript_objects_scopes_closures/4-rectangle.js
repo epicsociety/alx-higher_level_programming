c581ac8e1e882c7a243e2f3241dd5ba92aac7d5a
Epicsociety <measkswho@gmail.com>
2023-04-13 00:55:18 +0300

    defining the class Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+// a class rectangle
+// module.exports is an obj that holds exported values and
+// functions from that module
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rows = '';
+      for (let j = 0; j < this.width; j++) {
+        rows += 'X';
+      }
+      console.log(rows);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const Rectangle = require(`./4-rectangle`);
+
+// a class square
+// inherits from the class Rectangle
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
