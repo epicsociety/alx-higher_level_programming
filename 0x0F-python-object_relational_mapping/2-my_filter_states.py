1a774aff010105fb279c30350219b82d2a324f72
Epicsociety <measkswho@gmail.com>
2023-04-17 11:02:13 +0300

    list state searched using format

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''module that lists all states from the database'''
+
+import sys
+import MySQLdb
+
+
+def linkdatabase():
+    '''returns the database'''
+    database = MySQLdb.connect(host="localhost", port=3307, user=sys.argv[1],
+                               password=sys.argv[2], db=sys.argv[3])
+    return database
+
+
+def list_states(database):
+    '''executes the query'''
+    cur = database.cursor()
+
+    cur.execute(
+            "SELECT * FROM states WHERE BINARY name='{:s}' ORDER BY \
+            states.id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    database.close()
+
+
+if __name__ == '__main__':
+    list_states(linkdatabase())
