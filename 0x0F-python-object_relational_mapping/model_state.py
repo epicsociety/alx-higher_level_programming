0ca464a45204ad7a1a3ac171c0f942e5bd38573a
Epicsociety <measkswho@gmail.com>
2023-04-17 20:15:13 +0300

    class definition of a State and an instance Base = declarative_base()

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,5 +12,6 @@ Base = declarative_base()
 class State(Base):
     '''state class that inherits from Base'''
     __tablename__ = 'states'
-    id  = Column(Integer(11), primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer(11), primary_key=True, autoincrement=True,
+                nullable=False)
     name = Column(String(128), nullable=False)
