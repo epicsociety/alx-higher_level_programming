fb3d28a842b1eb057454fa32606261664ea2f0dc
Epicsociety <measkswho@gmail.com>
2023-04-17 13:20:23 +0300

    prints cities in a stated inputed by user in a one line

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''module lists all cities per their id and state
+'''module lists all cities in the state inputed by user
 and is safe form SQL injections
 '''
 
@@ -11,21 +11,26 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
     '''database conncection'''
     database = MySQLdb.connect(host='localhost', port=3306, user=username,
                                passwd=password, db=database_name)
 
     cur = database.cursor()
-    query = "SELECT cities.id, cities.name, states.name \
+    query = "SELECT cities.name\
             FROM cities \
             JOIN states ON cities.state_id = states.id \
+            WHERE states.name=%s \
             ORDER BY cities.id ASC"
-    cur.execute(query)
+    cur.execute(query, (state_name,))
     rows = cur.fetchall()
 
+    city_list = []
     for row in rows:
-        print(row)
+        city_list.append(row[0])
+    city_string = ', '.join(city_list)
+    print(city_string)
 
     cur.close()
     database.close()
