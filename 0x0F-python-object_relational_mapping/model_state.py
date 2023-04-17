0be62fa5a329488c4dfdce2f75439576db42b890
Epicsociety <measkswho@gmail.com>
2023-04-17 20:10:25 +0300

    class definition of a State and an instance Base = declarative_base()

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''module contain the State class'''
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''state class that inherits from Base'''
+    __tablename__ = 'states'
+    id  = Column(Integer(11), primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
