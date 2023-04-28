a371f9cfad06b803eb53f7e690ddc75851c0a326
Epicsociety <measkswho@gmail.com>
2023-04-28 14:39:51 +0300

    improvement of the code

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that sends a JSON POST request to URL passed, displays the body of the response
-curl -s -X POST -H "content_type:application/json" -d "@$2" "$1"
+curl -s -X POST -H "content_type: application/json" -d "@$2" "$1"
