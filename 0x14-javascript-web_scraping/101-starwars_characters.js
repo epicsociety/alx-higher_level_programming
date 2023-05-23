b7fd0f926e521095a91b559521f5d198dd145697
Epicsociety <measkswho@gmail.com>
2023-05-23 23:27:34 +0300

    use promises to get character names in same order as the /films/ response list

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const charactersUrls = JSON.parse(body).characters;
+  const fetchCharacterPromises = [];
+
+  charactersUrls.forEach((character) => {
+    const fetchPromise = new Promise((resolve, reject) => {
+      request.get(character, function (error, response, body) {
+        if (error) {
+          console.error(error);
+          return;
+        }
+
+        const characterName = JSON.parse(body).name;
+        resolve(characterName);
+      });
+    });
+    fetchCharacterPromises.push(fetchPromise);
+  });
+
+  Promise.all(fetchCharacterPromises)
+    .then((characterNames) => {
+      characterNames.forEach((name) => {
+        console.log(name);
+      });
+    })
+    .catch((error) => {
+      console.erro(error);
+    });
+});
