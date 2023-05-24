dd6582c25b64aadc7d7b393d80f73d8fe7436bb9
Epicsociety <measkswho@gmail.com>
2023-05-24 11:45:03 +0300

    correct output confirmed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
-const characterId = 18;
 
-request.get(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-
-  const films = JSON.parse(body).results;
-  const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
-  console.log(count.length);
 });
