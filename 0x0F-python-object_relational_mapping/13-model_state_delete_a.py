3cd5e289277838355183d087466ec12b848ea847
Epicsociety <measkswho@gmail.com>
2023-05-25 15:25:41 +0300

    session.delete to remove a state

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" module has script that lists all State objects that
+contain the letter a from the database hbtn_0e_6_usa """
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    username, password, db_name = sys.argv[1:]
+
+    # dialect(flavor)+driver(dbAPI)://username:password@host:port/database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        if 'a' in state.name.lower():
+            session.delete(state)
+
+    session.commit()
+    session.close()
